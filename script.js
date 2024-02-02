3cf3de1eb2caff46f069ac13e57db7b4bdf47e08
N1ler4 <bahtiyorov.nozim@gmail.com>
2024-02-02 09:43:51 +0500

    Project

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let menu = document.querySelector(".menu")
-let katalog = document.querySelector(".katalog")
+let menu = document.querySelector(".menu");
+let katalog = document.querySelector(".katalog");
 
-menu.addEventListener('click' , () => {
+menu.addEventListener("click", () => {
+  if (katalog.classList.contains("katalog-none") == true) {
+    katalog.classList.remove("katalog-none");
+  } else {
+    katalog.classList.add("katalog-none");
+  }
+});
 
-    if(katalog.classList.contains('katalog-none') == true){
-        katalog.classList.remove('katalog-none')
-    }else{
-        katalog.classList.add('katalog-none')
-    }
 
-})
+
+
+let menuTwo = document.querySelector(".menuTwo");
+let dropdown = document.querySelector(".dropdown");
+
+menuTwo.addEventListener("click", () => {
+  if (dropdown.classList.contains("dropdown-none") == true) {
+    dropdown.classList.remove("dropdown-none");
+  } else {
+    dropdown.classList.add("dropdown-none");
+  }
+});
